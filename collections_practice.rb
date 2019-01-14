ff071ca750036c44a04a31cee707e7199a82e2e4
Kian Edalat <akedalat@gmail.com>
2019-01-14 10:10:49 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,59 @@
-# your code goes here
+def begins_with_r(array)
+  new_array = []
+  array.collect do |element|
+    if element.start_with?("r")
+      new_array << element
+    else return false
+    end
+  end
+return true
+end
+
+def contain_a(array)
+  new_array = []
+  array.collect do |element|
+    if element.include?("a")
+      new_array << element
+    end
+  end
+return new_array
+end
+
+def first_wa(array)
+
+  array.find do |element|
+    if element[0]=="w" && element[1]=="a"
+
+      return element
+    end
+  end
+
+end
+
+def remove_non_strings(array)
+new_array = []
+  array.each do |element|
+if element.is_a?(String)
+  new_array << element
+  end
+end
+return new_array
+
+end
+
+def count_elements(array)
+
+
+end
+
+def merge_data
+
+end
+
+def find_cool
+
+end
+
+def organize_schools
+
+end
